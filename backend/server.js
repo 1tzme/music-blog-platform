6b7f661f8b4af12a8b4c39d9930f2ec3cd4ecf92
1tzme <zakir.itsme@gmail.com>
2025-02-25 20:27:41 +0500

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const {getSpotifyData} = require('./api');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
